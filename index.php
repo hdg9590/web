9dcb9db9df13ea286f6c96d84710918db7928dac
hdg9590 <141809011+hdg9590@users.noreply.github.com>
2025-08-06 00:35:22 +0900

    index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+header("Cache-Control: no-cache, no-store, must-revalidate");
+header("Pragma: no-cache");
+header("Expires: 0");
+
 $is_logged_in = isset($_SESSION['username']);
 ?>
 
